d26247024fd4e28066d3503ab68c5bb7c92f19f9
akitohiko <raskisenpai@gmail.com>
2022-11-23 17:21:41 +0800

    push

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
             <h2>百度网盘 &rarr;</h2>
             <p>提取码: 63w7</p>
           </a>
-
+        
           <a
             href="https://cloud.189.cn/t/nuiU3qNz6ri2"
             className={styles.card}
@@ -38,24 +38,36 @@ export default function Home() {
             <h2>天翼云盘 &rarr;</h2>
             <p>访问码:5ei2</p>
           </a>
+        </div>
 
+        <div className={styles.grid}>
           <a
-            href="https://deltaaster-my.sharepoint.com/:f:/g/personal/arkitoaya_deltaaster_onmicrosoft_com/ErzZ871R85JOmID6aFgQQtgBvlgJKiaHNA02MKmxOLxXoA?e=IGrxOQ"
+            href="https://deltaaster-my.sharepoint.com/:f:/g/personal/arkitoaya_arkitosekai_net/ErzZ871R85JOmID6aFgQQtgB8sxD9kz6qniIZMm4tQsrZg?e=r0ocAz"
             className={styles.card}
             target="_blank"
           >
             <h2>OneDrive &rarr;</h2>
             <p>无需会员，速度看脸</p>
           </a>
+
+          <a
+            href="https://pan.quark.cn/s/d6f292670531"
+            className={styles.card}
+            target="_blank"
+          >
+            <h2>夸克网盘 &rarr;</h2>
+            <p></p>
+          </a>  
         </div>
+
         <p className={styles.description}>
           内含最新版：
           <br/>
           Ableton Live 11 & 10
           <br/>
-          iZotope RX 9 & 10
+          iZotope RX 10 & 9
           <br/>
-          （如果部分版本Bug比较多将会等下一个大版本更新后上架）
+          （为了留出空间，旧版本软件仅百度网盘收录）
         </p>
       </main>
 
